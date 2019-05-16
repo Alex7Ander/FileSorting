c96f6119124f706d5e0b4a4ca79589bd376785e0
Alex7Ander <alex.ander.1991@mail.ru>
2019-05-16 21:26:37 +0300

    Initial commit (empty project)

---
+++ CMakeFiles/FileSorting.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FileSorting.dir/main.cpp.o"
+  "FileSorting.pdb"
+  "FileSorting"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FileSorting.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
